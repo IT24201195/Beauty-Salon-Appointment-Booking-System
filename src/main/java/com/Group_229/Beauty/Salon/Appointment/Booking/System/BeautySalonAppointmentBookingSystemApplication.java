93cb35c5987ff3684001d84ff5df6512c38fb6c7
IT24201195 <IT24201195@my.sliit.lk>
2025-03-21 13:04:54 +0530

    Project setup

---
+++ src/main/java/com/Group_229/Beauty/Salon/Appointment/Booking/System/BeautySalonAppointmentBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Group_229.Beauty.Salon.Appointment.Booking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonAppointmentBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalonAppointmentBookingSystemApplication.class, args);
+	}
+
+}
