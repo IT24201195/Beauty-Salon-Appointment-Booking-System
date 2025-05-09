0019bd72b657e39909f90c843f0ea9ec49420856
it24102051 <it24102051@my.sliit.lk>
2025-05-09 14:53:57 +0530

    File storage Framework

---
+++ src/main/java/com/Mathra/Salon/filemanager/FileStorable.java
@@ -0,0 +1,34 @@
+package com.Mathra.Salon.filemanager;
+
+/**
+ * Interface for objects that can be stored in a file
+ * Implementations should provide methods to convert to/from a string
+ */
+public interface FileStorable {
+    
+    /**
+     * Convert the object to a string for file storage
+     * @return A string representation of the object for file storage
+     */
+    String toFileString();
+    
+    /**
+     * Populate the object from a file string
+     * @param fileString String representation from the file
+     * @return The populated object or null if invalid
+     */
+    FileStorable fromFileString(String fileString);
+    
+    /**
+     * Get the ID of the object
+     * @return The ID
+     */
+    Long getId();
+    
+    /**
+     * Set the ID of the object
+     * 
+     * @param id The ID to set
+     */
+    void setId(Long id);
+} 
